76fbe0befd66c91b533894f931bdb72fa03ae3d6
Nick Harvey <nickharveyonline@gmail.com>
2021-07-23 11:08:20 -0400

    test

--- app/pipelines.py
+++ app/pipelines.py
@@ -1,50 +1,43 @@
-import os
 import logging
 import time
 import python_pachyderm
 import streamlit as st
 
-# Connects to a pachyderm cluster on localhost:30650.
-# For other options, see the API docs.
-client = python_pachyderm.Client()
-
-
 class Jaruco:
+	def __init__(self):
+		self.client = python_pachyderm.Client()
 
-	def general_restore(uploaded_file):
+	def general_restore(self, uploaded_file):
+
+
 		"""Do a general restore on a photo that doesn't have cracks"""
 		filename = '/{}'.format(uploaded_file.name)
 		img_bytes = uploaded_file.getvalue()
 
-		with client.commit("general_restore_input", "master") as commit:
-			client.put_file_bytes(commit, filename, img_bytes)
+		with self.client.commit("general_restore_input", "master") as commit:
+			self.client.put_file_bytes(commit, filename, img_bytes)
 			pass
 
-		#TODO Build an actual progress function
-		with st.spinner(text='Restoring... (~20 Seconds)'):
-			time.sleep(20)
-			pass
-
-		#TODO Build an actual progress function
+		# TODO Build an actual progress function
 		with st.spinner(text='Restoring...'):
 			# Check Pachyderm Jobs
 			while True:
 				time.sleep(5)
 				try:
-					if all(job.state == 3 for job in client.list("general_restore")):
+					if all(job.state == 3 for job in self.client.list("general_restore")):
 						break
 				except Exception as e:
-					logging.error("job fetch failed: %s".format(e))
+					logging.error("job fetch failed: {}".format(e))
 			pass
 		pass
 
-	def general_restore_wcracks(uploaded_file):
+	def general_restore_wcracks(self, uploaded_file):
 		"""Do a general restore on a photo that does have cracks"""
 		filename = '/{}'.format(uploaded_file.name)
 		img_bytes = uploaded_file.getvalue()
 		
-		with client.commit("general_restore_w_cracks_input", "master") as commit:
-			client.put_file_bytes(commit, filename, img_bytes)
+		with self.client.commit("general_restore_w_cracks_input", "master") as commit:
+			self.client.put_file_bytes(commit, filename, img_bytes)
 
 		with st.spinner(text='Restoring... (~40 Seconds)'):
 			time.sleep(40)
