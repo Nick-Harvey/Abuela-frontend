b40c2234e906abed070f4affb290fe12172d2bc0
Nick Harvey <nickharveyonline@gmail.com>
2021-07-24 09:50:04 -0400

    refining objectstore

--- app/object_store.py
+++ app/object_store.py
@@ -3,8 +3,9 @@ import logging
 from google.cloud import storage
 from decouple import config
 
-#PROJECT = config('GCLOUD_PROJECT')
+# PROJECT = config('GCLOUD_PROJECT')
 CREDS = config('GOOGLE_APPLICATION_CREDENTIALS')
+
 
 class ObjectStore:
 	def __init__(self):
@@ -12,9 +13,9 @@ class ObjectStore:
 
 	def upload_blob(self, bucket_name, uploaded_file, destination_blob_name):
 		"""Uploads a file to the GCS bucket."""
-		#TODO Add something to check if the file has already be added in the last 60 seconds
+		# TODO Add something to check if the file has already be added in the last 60 seconds
 
-		#client = storage.Client()
+		# client = storage.Client()
 		bucket = self.client.get_bucket('abuela_input_images_dev')
 		blob = bucket.blob(destination_blob_name)
 
@@ -25,20 +26,22 @@ class ObjectStore:
 				)
 			)
 
-	def download_blob(self, bucket_name):
-	    """Downloads a blob from the bucket."""
-	    bucket_name = "abuela_output_images_dev"
-	    # destination_file_name = "local/path/to/file"
+	def download_blob(self, bucket_name, file):
+		"""Downloads a blob from the bucket."""
+		# dev_bucket_name = "abuela_output_images_dev"
+		# destination_file_name = "local/path/to/file"
 
-	    for blob in self.client.list_blobs(bucket_name, prefix='/final_output/'):
-	    	filename = blob.name
-	    	
-	    	try:
-	    		restored_image = blob.download_as_bytes()
+		for blob in self.client.list_blobs(bucket_name, prefix='/final_output/'):
+			filename = blob.name
+			
+			try:
+				restored_image = blob.download_as_bytes()
 
-		    	logging.info(
-				"File {} downloaded to localhost.".format(filename)
+				logging.info(
+					"File {} downloaded to localhost.".format(filename)
+				)
+				
 			except Exception as e:
 				logging.error("unable to download file: {}".format(e))
 	
-	    	return restored_image
+			return restored_image
