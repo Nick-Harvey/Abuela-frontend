23408882b34e14ca9cc4856627ef97d1522f90e0
Nick Harvey <nickharveyonline@gmail.com>
2021-08-02 20:21:49 -0400

    test

--- app/pipelines.py
+++ app/pipelines.py
@@ -26,19 +26,20 @@ class Jaruco:
             # Check Pachyderm Jobs
             while True:
                 for i in range(100):
-                    try:
-                        if all(job.state == 3 for job in self.client.list_job(
-                            "general_restore")
-                        ):
-                            progress_bar.progress(100)
-                            break
-
-                    except Exception as e:
-                        logging.error("job fetch failed: {}".format(e))
-
                     progress_bar.progress(i + 1)
                     status_text.text('Restoring {}'.format(uploaded_file.name))
-                    time.sleep(.2)
+                    time.sleep(1)
+                try:
+                    if all(job.state == 3 for job in self.client.list_job(
+                        "general_restore")
+                    ):
+                        progress_bar.progress(100)
+                        break
+
+                except Exception as e:
+                    logging.error("job fetch failed: {}".format(e))
+
+                    
             pass
         pass
 
