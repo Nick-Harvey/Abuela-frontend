7e7e020a8dd4fe09d3393959451586f4cb020e90
Nick Harvey <nickharveyonline@gmail.com>
2021-08-07 08:59:46 -0400

    testing

--- app/pipelines.py
+++ app/pipelines.py
@@ -21,7 +21,7 @@ class Jaruco:
             #         return True
 
             try:
-                if all(job.state == 3 for job in self.client.list_job(pipeline_name, commit_id)):
+                if all(job.state == 3 for job in self.client.list_job((pipeline_name, "master", commit_id))):
                     status_text.text('Finished Restoring {}'.format(uploaded_file.name))
                     progress_bar.progress(100)
                     return True
